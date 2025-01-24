ec786155501cc25cdf143e854a5ac246983588cc
Lxx367 <luciabraga2001@gmail.com>
2025-01-24 12:47:16 +0100

    commit

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -5,10 +5,13 @@ import { FechaComponent } from "../activitiesUtils/fecha/fecha.component";
 import { FormsModule } from '@angular/forms';
 import { ActivityCardComponent } from "../activitiesUtils/activity-card/activity-card.component";
 import { ModalAddActivityComponent } from "../activitiesUtils/modal-add-activity/modal-add-activity.component";
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-activities',
-  imports: [CommonModule, FechaComponent, FormsModule, ActivityCardComponent, ModalAddActivityComponent],
+  imports: [CommonModule, FechaComponent, FormsModule, ActivityCardComponent, ModalAddActivityComponent, MatDatepickerModule, MatNativeDateModule, MatInputModule],
   templateUrl: './activities.component.html',
   styleUrl: './activities.component.scss'
 })
